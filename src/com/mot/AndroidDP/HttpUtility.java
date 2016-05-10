e4733de1262ccc42ab7a3c34a0ea93712fb534a3
yxzhm <yxzhm@sohu.com>
2016-05-10 18:54:55 +0800

    add login

---
+++ src/com/mot/AndroidDP/HttpUtility.java
@@ -0,0 +1,75 @@
+package com.mot.AndroidDP;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by bkmr38 on 5/10/2016.
+ */
+public class HttpUtility {
+    public static String GetRequest(String url, String para) {
+
+        try {
+            URL u = new URL(url + "?" + para);
+            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            String context=getContent((InputStream) conn.getContent());
+            return context;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static String PostRequest(String url, String para) {
+        try {
+            URL u = new URL(url);
+            byte[] data = para.getBytes("UTF-8");
+            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+            conn.setRequestProperty("Accept", "*/*");
+            conn.setRequestProperty("UserAgent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko");
+            conn.setRequestProperty("ContentType", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length));
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            conn.getOutputStream().write(data);
+            int code = conn.getResponseCode();
+            String content = getContent(conn.getInputStream());
+            return content;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+    private static String getContent(InputStream stream) throws IOException {
+        InputStreamReader in = new InputStreamReader(stream);
+        BufferedReader buff = new BufferedReader(in);
+
+        StringBuilder contentBuffer = new StringBuilder();
+        String line = null;
+        try {
+            do {
+                line = buff.readLine();
+                if (line != null) {
+                    contentBuffer.append(line + "\r\n");
+                }
+            } while (line != null);
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return contentBuffer.toString();
+    }
+}
---
+++ src/com/mot/AndroidDP/Login.java
@@ -0,0 +1,15 @@
+package com.mot.AndroidDP;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bkmr38 on 5/10/2016.
+ */
+public class Login extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
