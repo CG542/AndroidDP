9945a8a3125081c1c7c32a7caac34904fddf8621
yxzhm <yxzhm@sohu.com>
2016-05-10 17:30:49 +0800

    Initial commit

---
+++ src/com/mot/AndroidDP/AndroidDP.java
@@ -0,0 +1,15 @@
+package com.mot.AndroidDP;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDP extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
